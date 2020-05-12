6e7363692a0ad1e99b92fb75babdb354a9b7ba2b
王俊 <1871015627@qq.com>
2020-05-12 16:47:33 +0800

    udpate

---
+++ Object/Class.py
@@ -0,0 +1,15 @@
+# 
+class fuck:
+    # 属性
+    name =''
+    age = ''
+    # 构造函数
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def deBugPrint(self):
+        print('self.name:{}'.format(self.name))
+obj  = fuck('wang',28)
+obj.deBugPrint()
+
+ 
