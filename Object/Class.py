03ca6a37a18f82d65669d49c947b20b0c77af1de
王俊 <1871015627@qq.com>
2020-05-15 16:50:15 +0800

    update:tuple

--- Object/Class.py
+++ Object/Class.py
@@ -1,15 +1,17 @@
 # 
 class fuck:
     # 属性
-    name =''
+    name = ''
     age = ''
+    
     # 构造函数
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
     def deBugPrint(self):
         print('self.name:{}'.format(self.name))
-obj  = fuck('wang',28)
-obj.deBugPrint()
 
- 
+
+obj = fuck('wang', 19)
+obj.deBugPrint()
--- dataType/Tuple.py
+++ dataType/Tuple.py
@@ -1,1 +1,8 @@
 # 元祖
+# 1.元祖定义了是不能修改的
+# 2.元祖可通过count和index 获取值
+fuck = (1, 2, 3,3)
+# count 统计指定数值在元祖出现次数
+print(fuck.count(3));
+# index: 指定值在元祖中第一次出现的位置 可指定范围 第二和第三参数指定
+print(fuck.index(3));
