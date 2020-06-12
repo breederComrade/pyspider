2b4fae501631d7b2d5e145ceed2eafbe19eec504
王俊 <1871015627@qq.com>
2020-06-12 09:23:11 +0800

    update:address

--- app/api/v1/address.py
+++ app/api/v1/address.py
@@ -6,4 +6,17 @@
   description: 
   
 """
+from app.core.error import Success
+from app.extensions.api_docs.redprint import Redprint
+from app.extensions.api_docs.v1 import address as api_doc
+from app.models.address import Address
 
+api = Redprint(name='address',description='配送地址',api_doc = api_doc)
+
+@api.route('/all', methods=['GET'])
+# @api.doc(auth=True)
+def get_all_address():
+    return  'xxx'
+    '''查询所有「配送信息」'''
+    address_list = Address.query.filter_by(user_id=g.user.id).all_by_wrap()
+    return Success(address_list)
